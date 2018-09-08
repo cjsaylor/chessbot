4020502b3c16b0f11bf4f603c59c19096e8c9e95
Chris Saylor <cjsaylor@gmail.com>
2018-09-08 07:08:42 -0400

    Created a web handler that uses an analyzer to redirect.

---
+++ analysis/handler.go
@@ -0,0 +1,47 @@
+package analysis
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/cjsaylor/chessbot/game"
+)
+
+// Analyzer should interact with an external service and return a link to view the analysis
+type Analyzer interface {
+	Analyze(*game.Game) (*url.URL, error)
+}
+
+// Handler is an http handler that will redirect the user to an analysis of their game
+type Handler struct {
+	gameStorage game.GameStorage
+	analyzer    Analyzer
+}
+
+// NewHTTPHandler returns an instance of an analysis endpoint handler
+func NewHTTPHandler(store game.GameStorage, analyzer Analyzer) *Handler {
+	return &Handler{
+		gameStorage: store,
+		analyzer:    analyzer,
+	}
+}
+
+func (a Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	gameID := r.URL.Query().Get("game_id")
+	gm, err := a.gameStorage.RetrieveGame(gameID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	analysisURL, err := a.analyzer.Analyze(gm)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, analysisURL.String(), http.StatusTemporaryRedirect)
+}
