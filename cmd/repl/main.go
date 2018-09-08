673115371d06f78ae08f71aaa2187d03f81a1c99
Chris Saylor <cjsaylor@gmail.com>
2018-09-08 07:08:07 -0400

    Updated the REPL to include saving the game to sqlite3 db.
    This allows more of the feature set to be tested without slack.
    Also adds a new command for showing the new export.

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -3,16 +3,33 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/cjsaylor/chessbot/game"
 )
 
-const foolsMate = "rnbqkbnr/pppp1ppp/8/8/5P2/8/PPPPP2P/RNBQKBNR b KQkq - 03"
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+// Generate a random string of A-Z chars with len = l
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(97, 122))
+	}
+	return string(bytes)
+}
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	fmt.Println("Game REPL")
 	fmt.Println("Note: piece colors may appear reversed on dark background terminals.")
+	gameID := randomString(20)
+	store, _ := game.NewSqliteStore("./chessbot.db")
+	fmt.Println("Game ID: " + gameID)
 	initialState := ""
 	if len(os.Args) > 1 {
 		initialState = os.Args[1]
@@ -24,13 +41,13 @@ func main() {
 	}
 	if string(initialState) != "" {
 		var err error
-		gm, err = game.NewGameFromFEN("1", string(initialState), players...)
+		gm, err = game.NewGameFromFEN(gameID, string(initialState), players...)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 	} else {
-		gm = game.NewGame("1", players...)
+		gm = game.NewGame(gameID, players...)
 
 	}
 	scanner := bufio.NewScanner(os.Stdin)
@@ -42,6 +59,10 @@ func main() {
 		switch scanner.Text() {
 		case "fen":
 			fmt.Println(gm.FEN())
+			fmt.Print("\n> ")
+			continue
+		case "export":
+			fmt.Println(gm.Export())
 			fmt.Print("\n> ")
 			continue
 		case "resign":
@@ -56,6 +77,7 @@ func main() {
 				continue
 			}
 		}
+		store.StoreGame(gameID, gm)
 		fmt.Println(gm)
 		if outcome := gm.Outcome(); outcome != "*" {
 			fmt.Println(gm.ResultText())
