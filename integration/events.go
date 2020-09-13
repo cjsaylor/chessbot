cb88ccbc9f69bf5c32e24c912059aa91e05d7a59
Chris Saylor <cjsaylor@gmail.com>
2020-09-13 12:30:16 -0400

    Lint update.

--- integration/events.go
+++ integration/events.go
@@ -231,13 +231,13 @@ func (s SlackHandler) handleChallengeCommand(gameID string, command *ChallengeCo
 			Fallback:   "Unable to accept the challenge.",
 			CallbackID: "challenge_response",
 			Actions: []slack.AttachmentAction{
-				slack.AttachmentAction{
+				{
 					Name:  "challenge",
 					Text:  "Accept Challenge",
 					Type:  "button",
 					Value: "accept",
 				},
-				slack.AttachmentAction{
+				{
 					Name:  "challenge",
 					Text:  "Decline",
 					Type:  "button",
@@ -302,15 +302,15 @@ func (s SlackHandler) handleTakebackCommand(gameID string, ev *slackevents.AppMe
 
 func getHelpAttachments() []slack.Attachment {
 	return []slack.Attachment{
-		slack.Attachment{
+		{
 			Title: "Challenge Player",
 			Text:  "To challenge a player, mention @chessbot and say \"challenge @player_to_challenge\".",
 		},
-		slack.Attachment{
+		{
 			Title: "Making a move",
 			Text:  "To make a move playing, mention @chessbot and say \"d2d4\" which are the grid position of the piece you wish to move and the destination.",
 		},
-		slack.Attachment{
+		{
 			Pretext:   "For additional help visit our website.",
 			Title:     "ChessBot Help",
 			TitleLink: "https://www.chris-saylor.com/chessbot",
