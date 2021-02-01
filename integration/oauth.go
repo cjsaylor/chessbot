247637d25626262bbf8d6a9caaaaf7a1f24d777d
Chris Saylor <cjsaylor@gmail.com>
2021-02-01 09:42:57 -0500

    Add compatibility for Slack's v2 oauth2 flow.
    The app will check the newer v2 endpoint and fallback if it was initiated with v1 flow.

--- integration/oauth.go
+++ integration/oauth.go
@@ -22,15 +22,35 @@ func (s SlackOauthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		log.Println("invalid oauth code requested")
 		return
 	}
-	resp, err := slack.GetOAuthResponse(http.DefaultClient, s.SlackClientID, s.SlackClientSecret, code, "")
-	if err != nil {
+
+	var teamID string
+	var token string
+	resp, err := slack.GetOAuthV2Response(http.DefaultClient, s.SlackClientID, s.SlackClientSecret, code, "")
+	// During transition to slack's oauth v2.
+	if err.Error() == "oauth_authorization_url_mismatch" {
 		log.Println(err)
+		log.Println("Attempting fallback to v1 of Slack's oauth")
+		respV1, err := slack.GetOAuthResponse(http.DefaultClient, s.SlackClientID, s.SlackClientSecret, code, "")
+		if err == nil {
+			teamID = respV1.TeamID
+			token = respV1.Bot.BotAccessToken
+		}
+	} else if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("There was an issue exchanging tokens with Slack. Please try again."))
 		return
+	} else {
+		teamID = resp.Team.ID
+		token = resp.AccessToken
 	}
-	err = s.AuthStore.StoreAuthToken(resp.TeamID, resp.Bot.BotAccessToken)
+
+	err = s.AuthStore.StoreAuthToken(teamID, token)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("There was an issue exchanging tokens with Slack. Please try again."))
+		return
 	}
 	http.Redirect(w, r, fmt.Sprintf("https://slack.com/app_redirect?app=%v", s.SlackAppID), http.StatusSeeOther)
 }
